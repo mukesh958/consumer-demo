333fefaabe68821f49ae2df10921609f77599cd1
mukesh958 <mukesh958@gmail.com>
2023-07-02 13:02:18 +0530

    First

---
+++ src/main/java/com/mak/consumer/config/SwaggerConfig.java
@@ -0,0 +1,43 @@
+package com.mak.consumer.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+
+@Configuration
+public class SwaggerConfig {
+
+	@Bean
+	public OpenAPI customOpenAPI() {
+	    return new OpenAPI()
+	    		.info(getInfo())
+	    		//.addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+	    		//.components(new Components().addSecuritySchemes("", securitySchemesItem))
+	    		;
+	}
+	private Info getInfo() {
+		Info info= new Info();
+		info.version("1.0.0");
+		info.title("Spring Boot Basic things.");
+		info.description("This Application contains all the Rest end point that is needed for basic operation.");
+		Contact contact= new Contact();
+		contact.setEmail("Mukesh958@gmail.com");
+		contact.setName("Mukesh Gupta");
+		contact.setUrl("It's dummy");
+		info.contact(contact);
+		
+		License license= new License();
+		license.setName("Dummy name");
+		license.setUrl("Dummy URL");
+		info.license(license);
+		
+		
+		return info;
+	}
+}
---
+++ src/main/java/com/mak/consumer/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.mak.consumer.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/service")
+public class TestController {
+	
+	@GetMapping("/getdata")
+	public String getData() {
+		return "Hi! How are you.";
+	}
+
+}
